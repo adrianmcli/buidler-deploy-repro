53850bc5be31e8ce5a5d812530325c3d1f168a07
Adrian Li <li.adrianmc@gmail.com>
2020-05-25 20:38:37 -0400

    clean up script

--- deploy/index.js
+++ deploy/index.js
@@ -1,16 +1,6 @@
-// We require the Buidler Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-// When running the script with `buidler run <script>` you'll find the Buidler
-// Runtime Environment's members available in the global scope.
 const bre = require("@nomiclabs/buidler");
 
 async function main() {
-  // Buidler always runs the compile task when running scripts through it.
-  // If this runs in a standalone fashion you may want to call compile manually
-  // to make sure everything is compiled
-  // await bre.run('compile');
-
-  // We get the contract to deploy
   const Greeter = await ethers.getContractFactory("Greeter");
   const greeter = await Greeter.deploy("Hello, Buidler!");
 
@@ -25,6 +15,4 @@ async function main() {
   await myToken.deployed();
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 module.exports = main;
